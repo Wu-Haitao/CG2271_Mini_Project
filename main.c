bd38d0e3ada6e510e3fa662469cd8a34db8d692b
Wu-Haitao <59434361+Wu-Haitao@users.noreply.github.com>
2020-10-18 18:20:08 +0800

    Add motor control

--- main.c
+++ main.c
@@ -6,6 +6,11 @@
 #define UART_RX_PORTE23 23
 #define MASK(x) (1 << (x)) 
 #define RED_LED 18
+#define PTB0_Pin 0
+#define PTB1_Pin 1
+#define PTB2_Pin 2
+#define PTB3_Pin 3
+#define FREQ_TO_MOD(x) (375000/x) //48000000 / 128 = 375000
 
 uint8_t global_rx;
 osMutexId_t myMutex;
@@ -19,6 +24,60 @@ void initGPIO(void)
 	PORTB->PCR[RED_LED] |= PORT_PCR_MUX(1);
 	// Set Data Direction Registers for PortB
 	PTB->PDDR |= MASK(RED_LED);
+}
+
+void initPWM(void)
+{
+	//enable clock for portB
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	
+	//configure pin mux to alt3
+	PORTB->PCR[PTB0_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB0_Pin] |= PORT_PCR_MUX(3);
+	
+	PORTB->PCR[PTB1_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB1_Pin] |= PORT_PCR_MUX(3);
+
+	PORTB->PCR[PTB2_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB2_Pin] |= PORT_PCR_MUX(3);
+	
+	PORTB->PCR[PTB3_Pin] &= ~PORT_PCR_MUX_MASK;
+	PORTB->PCR[PTB3_Pin] |= PORT_PCR_MUX(3);
+	
+	//enable clock for timer1 and timer2
+	SIM->SCGC6 |= SIM_SCGC6_TPM1_MASK;
+	SIM->SCGC6 |= SIM_SCGC6_TPM2_MASK;
+	
+	//select clock source for TPM
+	SIM->SOPT2 &= ~SIM_SOPT2_TPMSRC_MASK;
+	SIM->SOPT2 |= SIM_SOPT2_TPMSRC(1);
+	
+	TPM1->MOD = FREQ_TO_MOD(50);
+	TPM2->MOD = FREQ_TO_MOD(50);
+	
+	TPM1->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM1->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	TPM1->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	TPM2->SC &= ~((TPM_SC_CMOD_MASK) | (TPM_SC_PS_MASK));
+	TPM2->SC |= (TPM_SC_CMOD(1) | TPM_SC_PS(7));
+	TPM2->SC &= ~(TPM_SC_CPWMS_MASK);
+	
+	//channel0
+	TPM1_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//channel1
+	TPM1_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM1_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+	
+	//channel2
+	TPM2_C0SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+	TPM2_C0SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
+
+	//channel3
+	TPM2_C1SC &= ~((TPM_CnSC_ELSB_MASK) | (TPM_CnSC_ELSA_MASK) | (TPM_CnSC_MSB_MASK) | (TPM_CnSC_MSA_MASK));
+  TPM2_C1SC |= (TPM_CnSC_ELSB(1) | TPM_CnSC_MSB(1));
 }
 
 void initUART2(uint32_t baud_rate){
@@ -52,7 +111,9 @@ void initUART2(uint32_t baud_rate){
 void UART2_IRQHandler(void) {
 	NVIC_ClearPendingIRQ(UART2_IRQn);
 	if (UART2->S1 & UART_S1_RDRF_MASK) {
+		osMutexAcquire(myMutex, osWaitForever);
 		global_rx = UART2->D;
+		osMutexRelease(myMutex);
 	}
 }
 
@@ -70,19 +131,37 @@ void tBrain (void *argument) {
 
 //Controls motors
 void tMotorControl (void *argument) {
+	for (;;) {
+		osMutexAcquire(myMutex, osWaitForever);
+		if (global_rx & 0x8) {
+			TPM1_C0V = 0;
+			TPM1_C1V = FREQ_TO_MOD(50) * (global_rx & 0x7) / (0x7);
+		} else {
+			TPM1_C0V = FREQ_TO_MOD(50) * (global_rx & 0x7) / (0x7);
+			TPM1_C1V = 0;
+		}
+		if ((global_rx >> 4) & 0x8) {
+			TPM2_C0V = 0;
+			TPM2_C1V = FREQ_TO_MOD(50) * ((global_rx >> 4) & 0x7) / (0x7);
+		} else {
+			TPM2_C0V = FREQ_TO_MOD(50) * ((global_rx >> 4) & 0x7) / (0x7);
+			TPM2_C1V = 0;
+		}
+		osMutexRelease(myMutex);
+	}
 }
 
 //Controls LEDs
 void tLED (void *argument) {
 	for (;;) {
-		osMutexAcquire(myMutex, osWaitForever);
+		//osMutexAcquire(myMutex, osWaitForever);
 		//Only for test
-		if (global_rx) {
-			LED_On(RED_LED);
-		} else {
-			LED_Off(RED_LED);
-		}
-		osMutexRelease(myMutex);
+		//if (global_rx) {
+		//	LED_On(RED_LED);
+		//} else {
+		//	LED_Off(RED_LED);
+		//}
+		//osMutexRelease(myMutex);
 	}
 }
 
@@ -95,12 +174,13 @@ int main (void) {
   SystemCoreClockUpdate();
   initUART2(BAUD_RATE);
 	initGPIO();
+  initPWM();
 	LED_Off(RED_LED);
   osKernelInitialize();                 // Initialize CMSIS-RTOS
   myMutex = osMutexNew(NULL);
 	osThreadNew(tBrain, NULL, NULL);
   osThreadNew(tMotorControl, NULL, NULL);
-  osThreadNew(tLED, NULL, NULL);
+  //osThreadNew(tLED, NULL, NULL);
   osThreadNew(tAudio, NULL, NULL);
   osKernelStart();                      // Start thread execution
   for (;;) {}
